c905ab2942df086c25114256f69c4e6f876f013b
Alex Drenea <alexdrenea@hotmail.com>
2019-07-24 00:20:01 -0400

    sample change

--- sample/CosmosDb.Sample/Program.cs
+++ sample/CosmosDb.Sample/Program.cs
@@ -6,7 +6,7 @@ namespace CosmosDb.Sample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Sample!");
         }
     }
 }
